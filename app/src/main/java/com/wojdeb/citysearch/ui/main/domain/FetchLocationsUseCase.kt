244eb188c9175ec9e9e9c54f3e8d4d484841d72c
Wojciech Debicki <wojciech.debicki@gmail.com>
2022-09-16 18:33:49 +0200

    Sorting added

--- app/src/main/java/com/wojdeb/citysearch/ui/main/domain/FetchLocationsUseCase.kt
+++ app/src/main/java/com/wojdeb/citysearch/ui/main/domain/FetchLocationsUseCase.kt
@@ -5,5 +5,21 @@ import com.wojdeb.citysearch.networking.domain.toDomain
 import javax.inject.Inject
 
 class FetchLocationsUseCase @Inject constructor(private val geoNamesRepository: GeoNamesRepository) {
-    suspend fun execute(text: String) = geoNamesRepository.fetch(text).map { it.toDomain() }
+
+    suspend fun execute(text: String): List<Location> {
+        val items = geoNamesRepository.fetch(text).map { it.toDomain() }
+
+        val sorted: MutableList<Location> =
+            items.sortedWith(compareBy({ it.countryName }, { it.stateName }, { it.cityName }))
+                .toMutableList()
+
+        //We should use sorting for it, e.g. custom comparator
+        val usElements = sorted.filter { it.countryName == "United States" }
+
+        sorted.removeAll(usElements)
+        sorted.addAll(0, usElements)
+
+        return sorted
+    }
+
 }
