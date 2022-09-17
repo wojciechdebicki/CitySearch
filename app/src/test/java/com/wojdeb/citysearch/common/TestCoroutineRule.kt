5cd285efd8af70b322406be64d86a62c2de26b16
Wojciech Debicki <wojciech.debicki@gmail.com>
2022-09-17 08:56:51 +0200

    Migrated to 1.6.0

--- app/src/test/java/com/wojdeb/citysearch/common/TestCoroutineRule.kt
+++ app/src/test/java/com/wojdeb/citysearch/common/TestCoroutineRule.kt
@@ -2,17 +2,16 @@ package com.wojdeb.citysearch.common
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.*
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
 import org.junit.rules.TestRule
 import org.junit.runner.Description
 import org.junit.runners.model.Statement
 
 @ExperimentalCoroutinesApi
 class TestCoroutineRule : TestRule {
-    //Yeah, deprecated - should me migrated
-    private val testCoroutineDispatcher = TestCoroutineDispatcher()
-
-    private val testCoroutineScope = TestCoroutineScope(testCoroutineDispatcher)
+    private val testCoroutineDispatcher = UnconfinedTestDispatcher()
 
     override fun apply(base: Statement, description: Description) = object : Statement() {
         @Throws(Throwable::class)
@@ -22,11 +21,7 @@ class TestCoroutineRule : TestRule {
             base.evaluate()
 
             Dispatchers.resetMain()
-            testCoroutineScope.cleanupTestCoroutines()
         }
     }
-
-    fun runBlockingTest(block: suspend TestCoroutineScope.() -> Unit) =
-        testCoroutineScope.runBlockingTest { block() }
 
 }
